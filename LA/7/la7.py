837f029917ba1ed0f0665ead878449dc1a94ae4a
kira <geloborcena016@gmail.com>
2024-12-23 11:38:11 +0800

    Update la7.py

--- LA/7/la7.py
+++ LA/7/la7.py
@@ -1,1 +1,14 @@
+#LA7
+class Car:
+    def __init__(self, brand, color):
+        self.brand = brand
+        self.color = color
 
+car_obj = Car("Toyota", "White")        
+print(f"Original value: {car_obj.brand} , {car_obj.color}")
+
+car_obj.color = "blue"
+print(f"Original value: {car_obj.brand} , {car_obj.color}")
+
+kotse = Car("Missubibi" , "Black")
+print(f"Updated value: {kotse.brand} , {kotse.color}")
