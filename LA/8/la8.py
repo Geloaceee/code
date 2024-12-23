4deeeb0579234f84e2b3310f173b35786d9f8cf6
kira <geloborcena016@gmail.com>
2024-12-23 11:38:25 +0800

    Update la8.py

--- LA/8/la8.py
+++ LA/8/la8.py
@@ -1,1 +1,11 @@
-
+#LA8
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+libro = Book("Stephen kings" , "IT")    
+print(f"Title: {libro.author} Author: {libro.title}")      
+del libro.author
+#print(f"Title: {libro.author} Author: {libro.title}")  
+libro2 = Book("J.K. Rowling", "Harry Potter")
+print(f"Title: {libro2.author} Author: {libro2.title}")     
