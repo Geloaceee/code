afb675005dce240041fed0fcced663bebcc3b0ed
kira <geloborcena016@gmail.com>
2024-12-23 11:37:58 +0800

    Update la6.py

--- LA/6/la6.py
+++ LA/6/la6.py
@@ -1,1 +1,8 @@
-
+LA#6
+class Laptop:
+    def __init__(self,brand, model):
+        pass
+    def laptop_info(self):
+        pass
+asus = Laptop("asus", "ROG Zephyrus G14")
+print(asus.laptop_info())
