1cea2e66fee349d31ea0c1018dee035a1e9ca4e8
kira <geloborcena016@gmail.com>
2024-12-23 11:39:25 +0800

    Update la11.py

--- LA/11/la11.py
+++ LA/11/la11.py
@@ -1,1 +1,16 @@
+#LA11
+class Phone():
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+        
+    def describePhone(self):
+        print(f"{self.brand} {self.model}")
+        
 
+class Android(Phone):
+    def __init__(self, brand, model):
+        Phone.__init__(self, brand, model)
+        
+silpon = Android("Oppo", "reno 11 pro 5g")  
+silpon.describePhone()
