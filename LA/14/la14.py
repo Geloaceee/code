676106724d97e9c67c47ad036f028715b1c5b0c8
kira <geloborcena016@gmail.com>
2024-12-23 11:40:06 +0800

    Update la14.py

--- LA/14/la14.py
+++ LA/14/la14.py
@@ -1,1 +1,17 @@
+#LA14
+class Animal():
+    def __init__(self, name, type, can_swim):
+        self.name = name
+        self.type = type
 
+    def describeAnimal(self):
+        print(f"{self.name} is a {self.type} that can swim, {self.can_swim}")
+        
+
+class Fish(Animal):
+    def __init__(self, name, type, can_swim):
+        self.can_swim = True
+        super().__init__(name, type, can_swim)
+
+pshh = Fish("tapalia", "pshh", True)    
+pshh.describeAnimal()
