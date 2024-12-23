487d8828e84db278a04d90808600dafa1ec4103b
kira <geloborcena016@gmail.com>
2024-12-23 11:39:07 +0800

    Update la10.py

--- LA/10/la10.py
+++ LA/10/la10.py
@@ -1,1 +1,20 @@
+#LA10
+class Vehicle():
+    def __init__(self, brand, model, fuel_type):
+        self.brand = brand
+        self.model = model
+        self.fuel_type = fuel_type
+        
+    def describeVehicle(self):
+        print(f"{self.brand} {self.model} uses {self.fuel_type} gas")
 
+class Kotsi(Vehicle):
+    pass
+
+class Mowtor(Vehicle):
+    pass
+
+car1 = Kotsi("Ford", "F-150", "Gasoline")
+car1.describeVehicle()
+Meowtor = Mowtor("Kawasaki", "Ninja 400", "Gasoline")
+Meowtor.describeVehicle()
