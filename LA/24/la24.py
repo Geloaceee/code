d814c6c5aaed7e26de36809b23a4573d85d54c11
kira <geloborcena016@gmail.com>
2024-12-23 11:42:38 +0800

    Update la24.py

--- LA/24/la24.py
+++ LA/24/la24.py
@@ -1,1 +1,34 @@
+LA#24
+from abc import ABC, abstractmethod
 
+class GameCharacter(ABC):
+    @abstractmethod
+    def attack(self):
+        pass
+
+class Warrior(GameCharacter):
+    def attack(self):
+        print("Swings sword!")
+
+class Mage(GameCharacter):
+    def attack(self):
+        print("Casts a Lightning!")
+
+class Archer(GameCharacter):
+    def attack(self):
+        print("Shoots an Bullet!")
+
+warrior = Warrior()
+mage = Mage()
+marksman = Marksman()
+
+warrior.attack()
+mage.attack()
+marksman.attack()
+
+class Healer(GameCharacter):
+    def attack(self):
+        print("Casts a healing spell on ally!")
+
+healer = Healer()
+healer.attack()
