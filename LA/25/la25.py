a135b3fc1202288690bd0e534476b7ad435c0493
kira <geloborcena016@gmail.com>
2024-12-23 11:42:50 +0800

    Update la25.py

--- LA/25/la25.py
+++ LA/25/la25.py
@@ -1,1 +1,21 @@
+LA#25
+from abc import ABC, abstractmethod
 
+class Character(ABC):
+    @property   
+    @abstractmethod
+    def __init__(self, name):
+        pass
+        
+    
+class Batman(Character):
+    def __init__(self, name, alias):
+        self.name = name
+        self.__alias = alias
+      
+    @property
+    def alias(self):
+        return self.__alias
+    
+Ichigo = Ichigo("Kurosaki", "Ichigo")
+print(Ichigo.alias)
