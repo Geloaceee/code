22aea543487167c7493c5969ead677a6087283e4
kira <geloborcena016@gmail.com>
2024-12-23 11:43:06 +0800

    Update la26.py

--- LA/26/la26.py
+++ LA/26/la26.py
@@ -1,1 +1,56 @@
+LA#26
+from abc import ABC, abstractmethod
 
+class PowerRangers(ABC):
+    @property
+    @abstractmethod
+    def alias(self):
+        pass
+
+class Red(PowerRangers):
+    def __init__(self, real_name, __alias):
+        self.real_name = real_name
+        self.__alias = __alias
+
+    @property
+    def alias(self):
+        return f"{self.real_name}, {self.__alias}"
+   
+class Yellow(PowerRangers):
+    def __init__(self, real_name, __alias):
+        self.real_name = real_name
+        self.__alias = __alias
+
+    @property
+    def alias(self):
+        return f"{self.real_name}, {self.__alias}"
+
+class Blue(PowerRangers):
+    def __init__(self, real_name, __alias):
+        self.real_name = real_name
+        self.__alias = __alias
+
+    @property
+    def alias(self):
+        return f"{self.real_name}, {self.__alias}"
+   
+class Pink(PowerRangers):
+    def __init__(self, real_name, __alias):
+        self.real_name = real_name
+        self.__alias = __alias
+
+    @property
+    def alias(self):
+        return f"{self.real_name}, {self.__alias}"
+
+import oop
+
+red = oop.Red("Red", "Redflag")
+yellow = oop.Yellow("Yellow", "Yellowflag")
+blue = oop.Blue("Blue", "Bluesky")
+pink = oop.Pink("Pink", "pinkflag")
+
+print(red.alias)
+print(yellow.alias)
+print(blue.alias)
+print(pink.alias)
