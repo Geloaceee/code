8b56edb737eb01d61c3fe2c65c76aa1e82fb5f87
kira <geloborcena016@gmail.com>
2024-12-23 11:37:40 +0800

    Update la5.py

--- LA/5/la5.py
+++ LA/5/la5.py
@@ -1,1 +1,16 @@
+LA#5
+class heroes:
+    def __init__(self, name, role, damage_type):
+        self.name = name
+        self.role = role
+        self.damage_type = damage_type
 
+    def description(self, role, damage_type):
+        print(f"{self.name} is a {role} that deals {damage_type} damage.")    
+
+#init
+super_hero = heroes("nana", "mage", "magic")  
+print(f"{super_hero.name} is a {super_hero.role} that deals {super_hero.damage_type} damage.")
+#desrcription
+super_hero = heroes("chou", "fighter", "physical")
+super_hero.description("fighter", "physical")
