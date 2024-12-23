8624df29a712151c7fda1fc272b9cd4bab8d4c63
kira <geloborcena016@gmail.com>
2024-12-23 11:42:23 +0800

    Update la23.py

--- LA/23/la23.py
+++ LA/23/la23.py
@@ -1,1 +1,19 @@
+LA#23
+class AnimeCharacter:
+    def __init__(self, name, ability):
+        self.name = name
+        self.ability = ability
 
+    def introduce(self, func):
+        def wrapper():
+            print("Introducing...")
+            func()
+            print("This character can one punch his enemy!")
+        return wrapper
+
+Saitama = AnimeCharacter("Saitama", "Fist")
+@Saitama.introduce
+def character_intro():
+    print(f"I am {Saitama.name} and I can use {Fist.ability}.")
+
+character_intro()
