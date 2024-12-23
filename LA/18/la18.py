55b7379a69ca850270758ca663c7e524a0f293bd
kira <geloborcena016@gmail.com>
2024-12-23 11:40:57 +0800

    Update la18.py

--- LA/18/la18.py
+++ LA/18/la18.py
@@ -1,1 +1,17 @@
-
+LA#18
+class Sikenkeri:
+    def __init__(self, meat,dry_season, wet_season):
+        self.meat = meat
+        self.dry_season = dry_season
+        self.wet_season = wet_season
+        
+    
+    def __str__(self):
+        return f"Ang aking Sikenkeri ko ay {self.meat}, {self.dry_season}, { self.wet_season}"
+    
+Sikenkeri_dry = Sikenkeri("Siken", "wator", "powder")
+Sikenkeri2 = Sikenkeri("Baboy", "wotoh" , "Dilis")
+Sikenkeri3 = Sikenkeri("Baka", "water" , "Luya")
+print(Sikenkeri_dry)
+print(Sikenkeri2)
+print(Sikenkeri3)
