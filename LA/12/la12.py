926832cdde50502764531907954a30a11e133832
kira <geloborcena016@gmail.com>
2024-12-23 11:39:39 +0800

    Update la12.py

--- LA/12/la12.py
+++ LA/12/la12.py
@@ -1,1 +1,15 @@
+#LA12
+class Toy():
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+        
+    def describeToy(self):
+        print(f"{self.name}, {self.price}")
 
+class Car(Toy):
+    def __init__(self, name, price):
+        super().__init__(name, price)
+
+kotse = Car("Honda", "500,000")
+kotse.describeToy()
