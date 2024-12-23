9b7476461efeb05645416a392d4f5639bc35b75c
kira <geloborcena016@gmail.com>
2024-12-23 11:38:41 +0800

    Update la9.py

--- LA/9/la9.py
+++ LA/9/la9.py
@@ -1,1 +1,12 @@
+#LA9
+class Car:
+    def __init__(self, brand):
+        self.brand = brand
 
+    def __str__(self):
+        return (f"{self.brand} is a car brand.")    
+    
+kotchi = Car("Toyota")
+print(kotchi.brand)
+del kotchi
+print(kotchi)
